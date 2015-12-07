c4231270de833f16231e3d3e65d1fce2f28226fd
Tom Mackall <mackall.tom@gmail.com>
2015-12-06 19:11:53 -0700

    btree03.py  - added intertive height calc.

--- btree03.py
+++ btree03.py
@@ -40,7 +40,6 @@ class btree(object):
     # thane tree and print.
         if btree.left is not None:
             cls.tprint(btree.left)
-        print btree.val
         if btree.right is not None:
             cls.tprint(btree.right)
                 # traverse right and recurse until the end
@@ -55,6 +54,31 @@ class btree(object):
             return 0
         return 1 + max(cls.get_height(node.left),cls.get_height(node.right))
 
+    @classmethod
+    def get_height_it(cls, node):
+        # init stack with root and set height to 0
+        level_queue = []
+        level_queue.append(node)
+        height = 0
+        # outer loop - for each level
+        while len(level_queue) > 0:
+            # increment height
+            height += 1
+            # set # of nodes to length of the queue
+            nodes = len(level_queue)
+            # inner loop - process all nodes on the level
+            while nodes > 0:
+                # pop off the queue
+                tmp_node = level_queue.pop(0)
+                # push next level onto the queue
+                if tmp_node.left is not None:
+                    level_queue.append(tmp_node.left)
+                if tmp_node.right is not None:
+                    level_queue.append(tmp_node.right)
+                # decrement nodes
+                nodes -= 1
+        return height
+
 
 size = 10
 sample = [60, 24, 47, 40, 85, 63, 25, 52, 78, 84, 5]
@@ -67,5 +91,6 @@ for i in sample:
 
 btree.tprint(tree)
 print btree.get_height(tree)
+print btree.get_height_it(tree)
 exit(0)
 
