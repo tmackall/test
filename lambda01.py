41badccfab8b32cb19c104648c6cf7837131880f
Tom Mackall <mackall.tom@gmail.com>
2015-12-19 08:42:33 -0700

    lambda01.py - initial revision.

---
+++ lambda01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+
+# lambda functions are one-off/custom functions that are used solely for a custom purpose
+# lambda can be put inline so they are close to
+my_list = [2, 4, 8, 9, 16]
+print  map(lambda x: x >> 1, my_list)
+
