c644d3894a10533862c6d621c51bc7a861d90440
Tom Mackall <mackall.tom@gmail.com>
2016-03-07 08:15:51 -0700

    Updated abc01.py with some tests.

--- abc01.py
+++ abc01.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 import abc
+
 
 class PluginBase(object):
     __metaclass__ = abc.ABCMeta
@@ -12,3 +15,27 @@ class PluginBase(object):
     def save(self, output, data):
         """Save the data object to the output."""
         return
+
+
+class Test(PluginBase):
+    pass
+
+
+try:
+    tc = Test()
+except TypeError:
+    print 'Cannot instantiate an abstract baseclass'
+
+
+class Test(PluginBase):
+
+    def __init__(self):
+        print 'successful instantiation'
+
+    def load(self, input):
+        pass
+
+    def save(self, output, data):
+        pass
+
+tc = Test()
